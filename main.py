2bf11af1f1a51e131bd9d0677cf48d680ed0e934
Jason Washburn <wburn@wburn.net>
2021-02-23 06:29:23 -0600

    added race functionality

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
-screen.setup(width=500,height=400)
+screen.setup(width=500, height=400)
 turtle_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+turtle_start_y = [75, 45, 15, -15, -45, -75]
 
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+while user_bet.lower() not in turtle_colors:
+    user_bet = screen.textinput(title="Make your bet", prompt="Please enter a valid color (red, orange, yellow,"
+                                                              " green, blue, purple)")
 
-tim = Turtle(shape='turtle')
-tim.color('red')
-tim.penup()
-tim.goto(-230, 60)
+turtles = [Turtle(shape='turtle') for color in turtle_colors]
+for i, turtle in enumerate(turtles):
+    turtles[i].penup()
+    turtles[i].color(turtle_colors[i])
+    turtles[i].goto(-230, turtle_start_y[i])
 
-tom = Turtle(shape='turtle')
-tom.color('orange')
-tom.penup()
-tom.goto(-230, 40)
+if user_bet:
+    is_race_on = True
 
-ted = Turtle(shape='turtle')
-ted.color('yellow')
-ted.penup()
-ted.goto(-230, 20)
+winner = "no"
+while is_race_on:
+    for turtle in turtles:
+        turtle.forward(random.randint(0, 10))
+        if turtle.xcor() >= 230:
+            winner = turtle
+            is_race_on = False
+            break
 
-tucker = Turtle(shape='turtle')
-tucker.color('green')
-tucker.penup()
-tucker.goto(-230, 0)
-
-teddy = Turtle(shape='turtle')
-teddy.color('blue')
-teddy.penup()
-teddy.goto(-230, -20)
-
-timmy = Turtle(shape='turtle')
-timmy.color('purple')
-timmy.penup()
-timmy.goto(-230, -40)
+if user_bet.lower() == winner.color()[0]:
+    print("You won!")
+else:
+    print("You lost.")
+print(f"The {winner.color()[0]} turtle is the winner!")
 
 screen.exitonclick()
